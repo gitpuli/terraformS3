5ce8435cf4774072085a77ce543c3aa28c01fba0
Sucharita Puli <psucharita@yahoo.com>
2020-07-25 18:05:44 -0700

    region commit

--- inputvariables.tf
+++ inputvariables.tf
@@ -18,45 +18,47 @@ variable acl {
         type = string 
     }
 
-variable cores { 
-    type = number 
-    }
+
+
 variable instance_name { 
     type = string 
 }
-variable db_name { 
-    type = string 
-    }
-variable storage_gb { 
-    type = number 
-    }
-variable publicly_accessible { 
-    type = bool 
-    default = true
-    }
-variable multi_az { 
-    type = bool 
-    default = true
-    }
-variable instance_class { 
-    type = string 
-    }
-variable engine { 
-    type = string 
-    }
-variable engine_version { 
-    type = string 
-    }
+# variable db_name { 
+#     type = string 
+#     }
+# variable storage_gb { 
+#     type = number 
+#     }
+# variable publicly_accessible { 
+#     type = bool 
+#     default = true
+#     }
+# variable multi_az { 
+#     type = bool 
+#     default = true
+#     }
+# variable instance_class { 
+#     type = string 
+#     }
+# variable engine { 
+#     type = string 
+#     }
+# variable engine_version { 
+#     type = string 
+#     }
 
-    variable mysql_db_name { 
-        type = string 
-        }
-variable mysql_hostname { 
-    type = string 
-    }
-variable mysql_port { 
-    type = number 
-    }
+#     variable mysql_db_name { 
+#         type = string 
+#         }
+# variable mysql_hostname { 
+#     type = string 
+#     }
+# variable mysql_port { 
+#     type = number 
+#     }
+#     variable cores { 
+#     type = number 
+#     }
 variable admin_username {
      type = string 
      }
